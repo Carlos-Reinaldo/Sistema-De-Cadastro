7f88602fc80a25854c52dc063db08178f49ed792
Carlos Reinaldo <berg.junior.jj@gmail.com>
2022-08-26 10:59:29 -0300

    Enviando para o GitHub

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
  console.log("Rodando o sistema de gerenciamento de códigos")
- console.log("aplicacao esta se conectando ao banco de dados")
+ console.log("aplicação esta se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula")
